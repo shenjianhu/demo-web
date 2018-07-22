3267083249f2e2510676e216417e2e69a78cdbd9
hushenjian <1213522415@qq.com>
2018-07-23 00:49:58 +0800

    add velocity

--- demo-controller/src/main/java/com/hsj/user/controller/UserController.java
+++ demo-controller/src/main/java/com/hsj/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.hsj.user.UserService;
 import com.hsj.user.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -38,5 +39,26 @@ public class UserController {
             System.out.println("登录失败");
             return "error";
         }
+    }
+
+    @RequestMapping(value = "/login1")
+    public ModelAndView login1(User user, HttpServletRequest request){
+        ModelAndView mv = new ModelAndView();
+        try {
+            User curUser = userService.login(user);
+            if(user == null){
+                mv.addObject("message", "用户不存在或者密码错误！请重新输入");
+                mv.setViewName("error");
+            }else{
+                mv.addObject("user", user);
+                mv.setViewName("userinfo");
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println("登录失败");
+        }
+        return mv;
     }
 }
